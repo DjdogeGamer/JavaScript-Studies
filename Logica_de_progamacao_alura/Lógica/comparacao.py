8b028440b4a8465b01b30b61e7f58bc3d0021a59
Gustavo Henrique <gustavohco34@gmail.com>
2022-01-06 03:13:32 -0300

    first commit

---
+++ Logica_de_progamacao_alura/Lógica/comparacao.py
@@ -0,0 +1,52 @@
+#########
+
+if n1 > n2 and n1 > n3:
+    m = n1
+else:
+    mn = n1
+
+if n2 > n3 and n2 > n1:
+    m = n2
+else:
+    mn = n2
+
+if n3 > n1 and n3 > n2:
+    m = n3
+else:
+    mn = n3
+
+#########
+
+if n3 > n2 and n3 > n1:
+    m = n3
+else:
+    mn = n3
+
+if n1 < n2 < n3:
+    m = n2
+else:
+    mn = n2
+
+if n1 > n3 and n1 > n2:
+    m = n1
+else:
+    mn = n1
+
+#########
+
+if n2 > n1 and n2 > n3:
+    m = n2
+else:
+    mn = n2
+
+if n1 > n2 > n3:
+    m = n1
+else:
+    mn = n1
+
+if n3 > n1 and n3 > n2:
+    m = n3
+else:
+    mn = n3
+
+#########
